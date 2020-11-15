4686d134a3698a68517a76ec3bb086d351d954ca
Teddy-XiongGZ <xgz18@mails.tsinghua.edu.cn>
2020-11-15 16:43:32 +0800

    Update config.py

--- HW2/config.py
+++ HW2/config.py
@@ -8,24 +8,45 @@ import torch
 from torch import nn
 import torch.optim as optim
 import torch.nn.functional as F
+import random
+
+class MyAugmentation(object):
+    """Augment a image tensor or a batch of image tensors
+        Uses the below transforms randomly
+    """
+    def __call__(self, image): 
+        image = transforms.RandomHorizontalFlip()(image)
+        image = transforms.RandomGrayscale()(image)
+        if random.random() < 0.5:
+            image = transforms.RandomRotation(15)(image)
+        # I didn't add cropping because it might hurt ImageNet-A performance 
+        # image = image.RandomCrop(64, pad_if_needed=True)
+        return image
 
 class Config(object):
     def __init__(self, model_name):
         self.batch_size = 100
         self.test_batch_size = 50
-        self.max_epoch = 30
+        self.max_epoch = 40
         self.class_num = 100
         self.learning_rate = 0.005
         self.drop_rate = 0.1
-        self.test_epoch = 2#4
+        self.test_epoch = 1
         self.save_path = './checkpoint'
         if not os.path.exists(self.save_path):
             os.mkdir(self.save_path)
         self.model_name = model_name
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.aug = MyAugmentation()
     
+    def set_batch_size(self,batch_size):
+        self.batch_size = batch_size
+
+    def set_test_batch_size(self,test_batch_size):
+        self.test_batch_size = test_batch_size
+
     def set_learning_rate(self,learning_rate):
-            self.learning_rate = learning_rate
+        self.learning_rate = learning_rate
 
     def logging(self, s, print_=True, log_=True):
         if print_:
@@ -49,8 +70,10 @@ class Config(object):
             train_total = train_correct = 0
 
             for data,target in train_loader:
-                data = data.to(self.device)                                             #这样会导致内存泄漏从而爆显存吗？
+                data = data.to(self.device)
+                data = torch.cat([data, self.aug(data)], 0)
                 target = target.to(self.device)
+                target = torch.cat([target, target], 0)
                 optimizer.zero_grad()
                 output = model(data)#得到预测值
 
@@ -63,6 +86,8 @@ class Config(object):
                 pred = torch.argmax(output, dim=-1).cpu()
                 train_total += len(pred)
                 train_correct += int(torch.sum(pred == target.squeeze(-1).cpu()))
+
+            # scheduler.step()
 
             train_loss = train_loss / len(train_loader.dataset)
             losses.append(train_loss)
@@ -78,9 +103,14 @@ class Config(object):
                 model.eval()
                 dataset.validate()
                 val_acc = self.test(model, dataset)
-                self.logging('Test on val_set\nTime: {:.2f}min; Test Accuracy: {:.4f}'.format((time.time()-eval_start_time)/60,val_acc))
-                if val_acc > best_acc:
-                    best_acc = val_acc
+                dataset.validate2()
+                val2_acc = self.test(model, dataset)
+                acc = (val_acc+val2_acc)/2
+                self.logging('Test Accuracy on val_set: {:.4f}'.format(val_acc))
+                self.logging('Test Accuracy on val2_set: {:.4f}'.format(val2_acc))
+                self.logging('Average Test Accuracy: {:.4f} | Time: {:.2f}'.format(acc,(time.time()-eval_start_time)/60))
+                if acc > best_acc:
+                    best_acc = acc
                     best_epoch = epoch + 1
                     torch.save(model.state_dict(),os.path.join(self.save_path, self.model_name))
                 model.train()
